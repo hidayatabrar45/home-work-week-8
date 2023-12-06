f821981cf87b1b4091aa6bcaa494dc612f2095be
hidayatabrar45 <hidayatabrar45@gmail.com>
2023-12-06 22:03:56 +0700

    Delete seeding directory

--- seeding/seeding.sql
+++
@@ -1,9 +0,0 @@
---Soal 1: seeding untuk menambahkan 5 data baru pada data table actor
--- Menambah 5 data baru pada table actor
-    INSERT INTO actor (first_name, last_name)
-    VALUES 
-    ('Robert', 'Downey Jr'),
-    ('Jeremy', 'Jenner'),
-    ('Chris', 'Evans'),
-    ('Bruce', 'Banner'),
-    ('Paul', 'Rudd')
